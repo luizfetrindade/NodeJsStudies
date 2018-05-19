8c814b318dcc29696fab92797d4dd2b857e809b8
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-19 12:47:33 -0300

    Adiciona negociacao controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,29 @@
 class NegociacaoController {
-    adiciona(event){
-        event.preventDefault()
-        alert('Toma')
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document)
+        this._inputData = $('#data')
+        this._inputQuantidade = $('#quantidade')
+        this._inputValor = $('#valor')
+
+    }
+
+    adiciona(event) {
+        event.preventDefault()
+
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        )
+
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+        
     }
 }
