70c4df8a95c8c618a3c5138152f047aaa0c1c6aa
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-19 15:55:20 -0300

    refatora helpers

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,12 +1,12 @@
 class DateHelper {
     
-    dateToText(date) {
+    static dateToText(date) {
         return date.getDate()
             + '/' + (date.getMonth() + 1)
             + '/' + date.getFullYear()
     }
 
-    textToDate(text) {
+    static textToDate(text) {
         return new Date(...text.split('-').map((item, indice) => item - indice % 2))
     }
 
