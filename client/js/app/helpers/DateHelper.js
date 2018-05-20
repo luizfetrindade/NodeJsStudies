a34bf98da4de9b6b86a0187c2112fb47dbc0e8b3
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-20 00:17:32 -0300

    Cria template pra table

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -10,7 +10,7 @@ class DateHelper {
     }
 
     static textToDate(text) {
-        if (!/\d{4}-\d{2}-\d{2}/.test(text)) throw new Error('Date format must be yyyy-mm-dd')
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(text)) throw new Error('Date format must be yyyy-mm-dd')
         return new Date(...text.split('-').map((item, indice) => item - indice % 2))
     }
 
