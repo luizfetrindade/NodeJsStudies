90ce625f98ded6699f1c44f63ae740748582501c
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-19 23:19:49 -0300

    blinda lista de negociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,16 +13,16 @@ class NegociacaoController {
         event.preventDefault()
 
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._limpaFormulario()
+
+        console.log(this._listaNegociacoes.negociacoes)
+    }
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textToDate(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        )
-
-        this._listaNegociacoes.adiciona(negociacao)
-        this._limpaFormulario()
-    
-        console.log(this._listaNegociacoes.negociacoes)
+            this._inputValor.value)
     }
 
     _limpaFormulario() {
