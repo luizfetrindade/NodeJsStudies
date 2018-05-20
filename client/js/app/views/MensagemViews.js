9e27f45aa1e67cad3f0e109cc37ba2bf0054ed46
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-20 20:10:31 -0300

    Adiciona herança

--- client/js/app/views/MensagemViews.js
+++ client/js/app/views/MensagemViews.js
@@ -1,13 +1,9 @@
-class MensagemVIew{
+class MensagemVIew extends View{
     constructor(elemento){
-        this._elemento = elemento
-    }
+        super(elemento)
+    }   
 
     _template(model){
         return model.text ? `<p class="alert alert-info">${model.text}</p>` : '<p></p>'
-    }
-
-    update(model){
-        this._elemento.innerHTML = this._template(model)
     }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View{
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento)
     }
 
     _template(model) {
@@ -40,9 +40,4 @@ class NegociacoesView {
         
         `;
     }
-
-    update(model) {
-        this._elemento.innerHTML = this._template(model);
-    }
-
 }
