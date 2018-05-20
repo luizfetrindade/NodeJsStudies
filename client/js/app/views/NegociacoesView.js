2ff60b11b0ce3f0bf5f4bd549d4ebaade15fd442
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-20 01:20:11 -0300

    Adiciona Total

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -17,8 +17,8 @@ class NegociacoesView {
             </thead>
             
             <tbody>
-               ${model.negociacoes.map(n => {
-                return`
+               ${model.negociacoes.map(n =>
+                `
                     <tr>
                         <td>${DateHelper.dateToText(n.data)}</td>
                         <td>${n.quantidade}</td>
@@ -26,11 +26,15 @@ class NegociacoesView {
                         <td>${n.volume}</td>
                     </tr>
                 `
-            }).join('')}
+            ).join('')}
             
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                </td>
             </tfoot>
         </table>
         
